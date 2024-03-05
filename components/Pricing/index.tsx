1633a5dfe08e872c2d61a75ace772fc3a06e6854
wenyi <wyzhang00@gmail.com>
2024-03-05 08:03:04 +0800

    fix shipping days

--- components/Pricing/index.tsx
+++ components/Pricing/index.tsx
@@ -18,7 +18,7 @@ const Pricing = () => {
     </p>,
     "📷 Please attach at least three photos of your beloved pet from different angles (front, side, top, the more the better) in the email. These photos serve as the foundation for my craftsmanship and enable me to capture your pet's unique personality.",
     "🐈‍⬛ I will share photos of the finished piece for your approval before shipping. I won't proceed until I have your consent. I'm happy to make any necessary adjustments until you're completely satisfied.",
-    "✈️ Each piece typically takes 20-30 days to complete (excluding modifications) and will be shipped in about 10 days thereafter. Thank you for your patience during this process.",
+    "✈️ Each piece typically takes 20-40 days to complete (excluding modifications) and will be shipped in about 10 working days thereafter. Thank you for your patience during this process.",
     "💅 Please note that all my works are 100% custom-made for you. If you have specific requests, such as adding accessories like hats or charms, or adding your pet's distinctive markings, please let me know, and I'll be happy to accommodate them. As each handmade product is unique, I'm unable to support returns or exchanges after shipping.",
     "🎁 A needle-felted creation makes a truly heartfelt gift. Whether it's for a birthday, anniversary, or special occasion, I can create a memorable piece that will be cherished for years to come. Let me know if you require gift wrapping or a personalized note to accompany your order.",
   ];
