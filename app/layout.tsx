1a86a05285bcb56be0cf378e8be040bde35e6ad2
wenyi <wyzhang00@gmail.com>
2026-03-13 16:10:19 +0800

    add claude init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-"use client";
+import { Metadata } from "next";
 import { Providers } from "./providers";
-import Script from "next/script";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+import Analytics from "./Analytics";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+
+export const metadata: Metadata = {
+  title: "Customized Needle Felted Pets to Honor Your Beloved Ones",
+  description:
+    "Needle felted custom wool animal portrait, key-chain or sculpture. 100% handmade. Best Choice of gifts or for memories.",
+  icons: {
+    icon: { url: "/images/favicon.svg", type: "image/svg+xml" },
+  },
+};
 
 export default function RootLayout({
   children,
@@ -17,47 +26,9 @@ export default function RootLayout({
       <head>
         {/* stripe buy button. More info: https://docs.stripe.com/payment-links/buy-button?client=react */}
         <script async src="https://js.stripe.com/v3/buy-button.js"></script>
-        {/* Google Tag Manager code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5959DF4M');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager code */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-H427LSWEWR"
-          strategy="afterInteractive"
-        />
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-H427LSWEWR');
-            `,
-          }}
-        />
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5959DF4M"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        <Analytics />
         <Providers>
           <Header />
           {children}
